8b171cd797ee6990d0505c72eaca80083ec8e5b2
Sebastian Nowicki <sebnow@gmail.com>
2011-09-18 14:35:30 +0800

    Reorganize benchmarks
    The Dumper benchmark is fairly useless as it's not intended to be used
    in the same way.
    
    Rather than having specific benchmarks for different serialization
    formats (i.e. JSON), they are consolidated to encode.pl/decode.pl. This
    also separates encoding from decoding giving more visibility.

--- benchmark/types.pl
+++ benchmark/types.pl
@@ -5,12 +5,12 @@ use warnings;
 use Benchmark qw(cmpthese);
 use Text::TNetstrings qw(:all);
 
-cmpthese(100000, {
+cmpthese(-10, {
 	'null' => sub{decode_tnetstrings(encode_tnetstrings(undef))},
 	'string' => sub{decode_tnetstrings(encode_tnetstrings("hello"))},
 	'number' => sub{decode_tnetstrings(encode_tnetstrings(42))},
 	'float' => sub{decode_tnetstrings(encode_tnetstrings(3.14))},
-	'array' => sub{decode_tnetstrings(encode_tnetstrings(["hello"]))},
+	'array' => sub{decode_tnetstrings(encode_tnetstrings(["hello", "world"]))},
 	'hash' => sub{decode_tnetstrings(encode_tnetstrings({"hello" => "world"}))},
 });
 
